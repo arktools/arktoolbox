5559747634b5454d21080d98e920aa03f6a46ff0
James Goppert <jgoppert@l1.hsl.dynalias.com>
2011-08-27 00:48:18 -0400

    Renaming.

--- DefineOptions.cmake
+++
@@ -1,3 +0,0 @@
-option(WITH_OSG "Build with openscenegraph support" ON)
-option(WITH_SCICOSLAB "Build scicoslab toolbox" ON)
-option(WITH_TESTING "Build with unit tests" OFF)
--- cmake/arkcmakeUpdate.sh
+++ cmake/arkcmakeUpdate.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # script used to update cmake modules from git repo, can't make this
 # a submodule otherwise it won't know how to interpret the CMakeLists.txt
-git clone git@github.com:jgoppert/arkCMake.git tmp
-rm *.cmake
-rm -rf tmp/.git
-mv tmp/* .
-rm -rf tmp
+if [ -d arkcmake ]; then rm -rf arkcmake; fi
+git clone git@github.com:arktools/arkcmake.git arkcmake
+rm -rf arkcmake/*.git
