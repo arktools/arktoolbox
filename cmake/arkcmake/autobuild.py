562c72a6f1f817e7bef63a138531fdd90b0e1f28
James Goppert <jgoppert@w7.hsl.dynalias.com>
2011-08-30 11:36:05 -0400

    Updated cmake.

--- cmake/arkcmake/autobuild.py
+++ cmake/arkcmake/autobuild.py
@@ -2,18 +2,31 @@
 # Author: Lenna X. Peterson (github.com/lennax)
 # based on bash script by James Goppert (github.com/jgoppert)
 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# USAGE:                                                                      #
+# $ ./autobuild.py [1-9]                                                      #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# TODO: Error handling: 
+## check cmake success, etc.
+
 import sys # for sys.argv[] and sys.platform
 import os # for chdir()
 import subprocess # for check_call()
 
 makeargs = "-j8"
 cmakeargs = " "
+build_dir = "build"
 
 def install_build(cmakeargs):
 	if cmakeargs == " ":
 		print "You chose install build"
-	subprocess.check_call('mkdir -p build', shell=True)
-	os.chdir("build")
+	try: 
+		os.mkdir(build_dir)
+	except OSError as (errno, errstr):
+		if errno == 17: # File exists
+			pass 
+	os.chdir(build_dir)
 	cmake_call = "cmake" + cmakeargs + ".."
 	subprocess.check_call(cmake_call, shell=True)
 	subprocess.check_call(["make", makeargs])
@@ -27,9 +40,19 @@ def dev_build():
 def grab_deps():
 	print "You chose to install dependencies"
 	if 'linux' in sys.platform:
-		subprocess.check_call('sudo apt-get install cmake', shell=True)
+		try: 
+			subprocess.check_call('sudo apt-get install cmake', shell=True)
+		except: 
+			print "Error installing dependencies: ", sys.exc_info()[0]
+			print "apt-get is available on Debian and Ubuntu" 
+			raise SystemExit
 	elif 'darwin' in sys.platform:
-		subprocess.check_call('sudo port install cmake', shell=True)
+		try: 
+			subprocess.check_call('sudo port install cmake', shell=True)
+		except: 
+			print "Error installing dependencies: ", sys.exc_info()[0]
+			print "Please install Macports (http://www.macports.org)"
+			raise SystemExit
 	else: 
 		print "Platform not recognized (did not match linux or darwin)"
 	raise SystemExit
@@ -45,7 +68,7 @@ def profile():
 
 def remake():
 	print "You chose to recall make on the previously configured build"
-	os.chdir("build")
+	os.chdir(build_dir)
 	subprocess.check_call(["make", makeargs])
 	raise SystemExit
 
@@ -63,8 +86,12 @@ def package():
 
 def clean():
 	print "You chose to clean the build"
-	subprocess.check_call('rm -rf build', shell=True)
-	# menu()
+	for root, dirs, files in os.walk(build_dir, topdown=False): 
+		for name in files: 
+			os.remove(os.path.join(root, name))
+		for name in dirs: 
+			os.rmdir(os.path.join(root, name))
+	os.rmdir(build_dir)
 	
 def menu():
 	print "1. developer build: used for development."
@@ -79,34 +106,38 @@ def menu():
 	opt = raw_input("Please choose an option: ")
 	return opt
 
-loop_num = 0
-# continues until a function raises system exit
-while (1): 	
-	if len(sys.argv) == 2 and loop_num == 0:
-		opt = sys.argv[1]
-		loop_num += 1
-	else:
-		opt = menu()
+try: 
+	loop_num = 0
+	# continues until a function raises system exit
+	while (1): 	
+		if len(sys.argv) == 2 and loop_num == 0:
+			opt = sys.argv[1]
+			loop_num += 1
+		else:
+			opt = menu()
 
-	opt = int(opt)
-	if   opt == 1:
-		dev_build()
-	elif opt == 2:
-		install_build(cmakeargs)
-	elif opt == 3: 
-		grab_deps()
-	elif opt == 4:
-		package_source()
-	elif opt == 5:
-		package()
-	elif opt == 6:
-		remake()
-	elif opt == 7:
-		clean()
-	elif opt == 8:
-		# requires definition in CMakeLists.txt (see def above)
-		profile()
-	elif opt == 9:
-		raise SystemExit
-	else:
-		print "Invalid option. Please try again. " 
+		opt = int(opt)
+		if   opt == 1:
+			dev_build()
+		elif opt == 2:
+			install_build(cmakeargs)
+		elif opt == 3: 
+			grab_deps()
+		elif opt == 4:
+			package_source()
+		elif opt == 5:
+			package()
+		elif opt == 6:
+			remake()
+		elif opt == 7:
+			clean()
+		elif opt == 8:
+			# requires definition in CMakeLists.txt (see def above)
+			profile()
+		elif opt == 9:
+			raise SystemExit
+		else:
+			print "Invalid option. Please try again: " 
+		
+except KeyboardInterrupt: 
+	print "\n" 
