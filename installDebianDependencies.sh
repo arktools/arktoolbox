2f44afaa5510c397949c80ff4a69362a5a144aaa
James Goppert <jgoppert@w7.hsl.dynalias.com>
2011-09-02 15:34:31 -0400

    Added debian package install script.

---
+++ installDebianDependencies.sh
@@ -0,0 +1,5 @@
+echo you chose to install debian dependencies
+sudo apt-get install cmake libqt4-dev simgear-dev liblapack-dev libboost-all-dev libopenscenegraph-dev gfortran
+sudo apt-get install libgl1-mesa-dri libgl1-mesa-glx libglu1-mesa 
+sudo apt-get install automake libtool
+sudo apt-get install scicoslab-gtk
