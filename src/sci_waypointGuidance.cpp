47142ace2042a1193c87185273c574539d3bc4c9
andrewms <andrew.shull1@gmail.com>
2011-09-04 14:02:33 -0400

    Modified simulation block to accomodate obstacle. Added low pass filter to the bearing command error. Obstacle mitigation is working in that the vehicle will navigate around the obstacle, but is not in the sense that it pushes the vehicle into oscillations that it does not recover from. Navigation far from an obstacle is not affected.

--- src/sci_waypointGuidance.cpp
+++ src/sci_waypointGuidance.cpp
@@ -37,7 +37,9 @@ extern "C"
 #include <math.h>
 #include "definitions.hpp"
 
-#define EARTH_RAD 6378.137
+
+void vincentys(double, double, double, double,double*, double*); 
+
 
 void sci_waypointGuidance(scicos_block *block, scicos::enumScicosFlags flag)
 {
@@ -85,16 +87,26 @@ void sci_waypointGuidance(scicos_block *block, scicos::enumScicosFlags flag)
     if (flag==scicos::computeOutput)
     {
         // basic guidance to waypoint
-        double dLat = lat1-lat;
-        double dLon = lon1-lon;
+        double psi1;
+	    double c;
+        vincentys(lat, lon, lat1, lon1, &c, &psi1);
+
+        /*
         double y = sin(dLon) * cos(lat1);	
         double x = cos(lat)*sin(lat1) - sin(lat)*cos(lat1)*cos(dLon);
 	    double c = sqrt(x*x + y*y);
     	double ihat_v = x/c;
 	    double jhat_v = y/c;
         double psi1 = atan2(y,x);
+        */
 
         // basic safety zone collision avoidance
+        double rC = 10; // collision avoidance window, 10 meters
+        double dC;
+        double psiC;
+        vincentys(lat, lon, lat3, lon3, &dC, &psiC);
+
+        /*
         double rC = 10; // collision avoidance window, 10 meters
         double Vc = Vt3 - Vt;
         double dLatC = lat3-lat;
@@ -102,37 +114,28 @@ void sci_waypointGuidance(scicos_block *block, scicos::enumScicosFlags flag)
         double yC = sin(dLonC) * cos(lat3);
         double xC = cos(lat)*sin(lat3) - sin(lat)*cos(lat3)*cos(dLon);
         double dC = sqrt(xC*xC + yC*yC); // distance to collision
+        */
 
         double deltaV;
         double deltaPsi;
-        // Ignore obstacles farther than 10 km away
-    	if (dC > -1) {
+        // Ignore obstacles that are far away
+    	if (dC > 1000) {
         	deltaV = 0;
         	deltaPsi = 0;
         } else {
 
-        	double psiC = acos(xC/dC);
-	        if(yC < 0) {
-            	psiC = -1 * psiC;
-        	}
-
-        	double ihat_c = cos(psi3);
-        	double jhat_c = sin(psi3);
-
         	// Get the velocity vector of the vehicle relative to the obstacle
-        	double velx_vrelc = (Vt * ihat_v ) - (Vt3 * ihat_c);
-        	double vely_vrelc = (Vt * jhat_v) - (Vt3 * jhat_c);
+        	double velx_vrelc = (Vt * sin(psi1) ) - (Vt3 * sin(psi3));
+        	double vely_vrelc = (Vt * cos(psi1)) - (Vt3 * cos(psi3));
         	double velmag_vrelc = sqrt(velx_vrelc*velx_vrelc + vely_vrelc*vely_vrelc);
 
-        	// Get bearing of the relative velocity of the vehicle
-        	double psi_vrelc = acos(velx_vrelc / velmag_vrelc);
-        	if(vely_vrelc < 0) {
-        		psi_vrelc *= -1;
-        	}
+        	// Get the angle of the relative velocity of the vehicle
+        	double psi_vrelc = atan2(vely_vrelc, velx_vrelc);
 
-        	// Get the difference between a collision cource bearing and the
-        	// current bearing
-        	double alpha = psi_vrelc - psiC;
+        	// Get the difference between a collision course bearing and the
+        	// current bearing. (psiC is bearing from North, psi_vrelc is angle
+            // from the horizontal, so it has been shifted))
+        	double alpha = psi_vrelc - (psiC + M_PI/2);
         	if (alpha < -1*M_PI) {
         		alpha += 2*M_PI;
         	} else if (alpha > M_PI) {
@@ -141,78 +144,102 @@ void sci_waypointGuidance(scicos_block *block, scicos::enumScicosFlags flag)
 
         	double velx_vrelc_new;
         	double vely_vrelc_new;
-        	// Case where the separation distance is not already violated.
-        	if (dC > rC) {
-        		// Get the magnitude difference between a collision course
-        		// bearing and a bearing that maintains separation.
-        		double beta = asin(rC/dC);
-        		double gamma;
-		
-        		// The vehicle is not on a collision cource
-        		if(abs(alpha) > beta) {
-        			gamma = 0;
-        		} else {
-        			if(alpha < 0) {
-        				gamma = -1 * alpha - beta;
-        			} else {
-        				gamma = beta - alpha;
-        			}
-        		}
+            double beta = asin(rC/dC);
+            double gamma = 0;
 
-        		// shift the bearing of the relative velocity vector by gamma
-        		psi_vrelc += gamma;
-        		velx_vrelc_new = cos(psi_vrelc);
-        		vely_vrelc_new = sin(psi_vrelc);
-        	} 
-        	// The case where the separation is already violated
-        	else {
-		        // The new relative velocity vector should point directly
-        		// away from the obstacle.
-		        velx_vrelc_new = cos(-1*psiC);
-        		velx_vrelc_new = sin(-1*psiC);
-        	}
+            // If the vehicle is on a course that would violate separation
+            if (fabs(alpha) < beta) {
 
-        	// The unit vector of the desired relative velocity has now been determined.
-        	// Using this, the new bearing of the vehicle needs to be determined to
-        	// create the relative velocity in that direction while the magnitude of
-        	// the vehicles velocity remains constant.
+            	// Case where the separation distance is not already violated.
+            	if (dC > rC) {
+        		    if(alpha < 0) {
+            		    gamma = -1 * alpha - beta;
+            		} else {
+            			gamma = beta - alpha;
+        	   		}
 
-        	double psiv;
-        	double d,e;
-        	double vnew = Vt;
-        	// If the bearing is in the regions where the tangent of that angle is defined.
-        	if ((psi_vrelc > -1 * M_PI/4 && psi_vrelc < M_PI / 4) || (psi_vrelc > 3 * M_PI / 4 || psi_vrelc < -3*M_PI/4)) {
+            		// shift the bearing of the relative velocity vector by gamma
+            		psi_vrelc += gamma;
+                    if(psi_vrelc > M_PI) {
+                        psi_vrelc -= 2*M_PI;
+                    } else if (psi_vrelc < -M_PI) {
+                        psi_vrelc += 2*M_PI;
+                    }
 
-        		d = tan(psi_vrelc);
-		        e = (d*cos(psi3)-sin(psi3)) / sqrt(1+d*d);
-        		if(abs (e) > 1) {
-		        	vnew = Vt3 * e;
-        		}
-        		psiv = asin(Vt3/vnew*e) + psi_vrelc - M_PI;
-        	}
-        	else {
+        	    	velx_vrelc_new = cos(psi_vrelc);
+        		    vely_vrelc_new = sin(psi_vrelc);
+            	} 
+            	// The case where the separation is already violated
+            	else {
+		            // The new relative velocity vector should point directly
+        		    // away from the obstacle.
+    		        velx_vrelc_new = cos(-1*(psiC+ M_PI/2));
+            		vely_vrelc_new = sin(-1*(psiC + M_PI/2));
+            	}
 
-        		// take the cotangent (phase shifted tangent)
-        		d = tan(M_PI_2 - psi_vrelc);
-        		e = (cos(psi3)-d*sin(psi3))/sqrt(1+d*d);
-        		if (abs(e) > 1) {
-		        	vnew = Vt3*e;
-        		}
-		        psiv = asin(Vt3/vnew * e) + psi_vrelc - M_PI;
+        	    // The unit vector of the desired relative velocity has now been determined.
+            	// Using this, the new bearing of the vehicle needs to be determined to
+            	// create the relative velocity in that direction while the magnitude of
+            	// the vehicles velocity remains constant.
 
-        	}
+            	double psiv;
+               	double d,e;
+            	double vnew = Vt;
+        	    // If the bearing is in the regions where the tangent of that angle is defined.
+            	if ((psi_vrelc > -1 * M_PI/4 && psi_vrelc < M_PI / 4) || (psi_vrelc > 3 * M_PI / 4 || psi_vrelc < -3*M_PI/4)) {
+
+            		d = tan(psi_vrelc);
+		            e = (d*cos(psi3)-sin(psi3)) / sqrt(1+d*d);
+        	    	if(abs (e) > 1) {
+		            	vnew = Vt3 * e;                    
+            		} else {
+                        vnew = Vt;
+                    }
+        	    	psiv = asin(Vt3/vnew*e) + psi_vrelc;
+            	}
+            	else {
+
+            		// take the cotangent (phase shifted tangent)
+        	    	d = tan(M_PI_2 - psi_vrelc);
+        		    e = (cos(psi3)-d*sin(psi3))/sqrt(1+d*d);
+            		if (abs(e) > 1) {
+	    	        	vnew = Vt3*e;
+            		} else {
+                        vnew = Vt;
+                    }
+    		        psiv = asin(Vt3/vnew * e) + psi_vrelc;
+
+            	}
+
+                deltaV = vnew - Vt;
+                deltaPsi = psi - psiv;
+                if(deltaPsi > M_PI){
+                    deltaPsi -= 2*M_PI;
+                } else if (deltaPsi < -M_PI) {
+                    deltaPsi += 2*M_PI;
+                }
+            }
+            // The vehcile is not on a collision course
+            else {
+                deltaV = 0;
+                deltaPsi = 0;
+            }
+
         }
+
 
         // output
         eH = alt1 - alt;
-        eV = /*sqrt(Vt*Vt + (Vc*Vc*sin(gamma)*sin(gamma))) + */ Vt1 - Vt;
+        eV = /*sqrt(Vt*Vt + (Vc*Vc*sin(gamma)*sin(gamma))) + */ Vt1 + deltaV - Vt;
         eR = 0 - R;
         ePhi = 0 - phi;
         
-        ePsi = psi1 /*+ atan2(Vc*sin(gamma),Vt)*/- psi;
+        ePsi = psi1 + deltaPsi - psi; /*+ atan2(Vc*sin(gamma),Vt)*/
+        
+        ePsi = fmod(ePsi, 2*M_PI);
         if(ePsi > M_PI) {
             ePsi -= 2*M_PI;
-        } else if(ePsi < M_PI) {
+        } else if(ePsi < -M_PI) {
             ePsi += 2*M_PI;
         }
    }
@@ -228,5 +255,144 @@ void sci_waypointGuidance(scicos_block *block, scicos::enumScicosFlags flag)
     }
 }
 
-} // extern c
+}
+
+
+#define MAJOR_AXIS_LENGTH 6378137.0
+#define MINOR_AXIS_LENGTH 6356752.3142
+#define FLATTENING ( 1 / 298.257223563)
+#define ITERATION_LIMIT 100
+
+void vincentys(double lat1, double lon1, double lat2, double lon2,
+		double *distance, double *bearing) {
+
+	int iterationCount = 0;
+	double   L;
+	double   U1;
+    double   U2;
+	double   lambda;
+	double   sigma;
+	double   sinSigma;
+    double   cosSigma;
+    double   sinLambda;
+    double   cosLambda;
+    double   sinU1;
+    double   sinU2;
+    double   cosU1;
+    double   cosU2;
+	double   sinAlpha;
+    double   cosSquaredAlpha;
+	double   cos2SigmaM;
+	double   C;
+	double   A;
+    double   B;
+	double   uSquared;
+	double   deltaSigma;
+    double   lambdaP;
+    double   rad;
+    
+	L = lon2 - lon1;
+
+    U1  = atan((1 - FLATTENING) * tan(lat1));
+    U2  = atan((1 - FLATTENING) * tan(lat2));
+    
+    sinU1 = sin(U1);
+    sinU2 = sin(U2);
+    cosU1 = cos(U1);
+    cosU2 = cos(U2);
+
+	lambda = L;
+
+	do {
+        sinLambda = sin(lambda);
+        cosLambda = cos(lambda);
+        
+        sinSigma = sqrt(
+            (cosU2 * sinLambda) *
+            (cosU2 * sinLambda) +
+            (cosU1 * sinU2 - sinU1 * cosU2 * cosLambda) *
+            (cosU1 * sinU2 - sinU1 * cosU2 * cosLambda)
+            );
+            
+        if (sinSigma == 0) {
+            *distance = -1;
+            return;
+        }
+        
+        cosSigma = (sinU1 * sinU2) +
+            (cosU1 * cosU2 * cosLambda);
+            
+        sigma = atan2(sinSigma, cosSigma);
+        
+        sinAlpha        = cosU1 * cosU2 * sinLambda / sinSigma;
+        cosSquaredAlpha = 1 - sinAlpha * sinAlpha;
+        
+        cos2SigmaM = cosSigma - 2 * sinU1 * sinU2 / cosSquaredAlpha;
+        
+        if (isnan(cos2SigmaM)) {
+            cos2SigmaM = 0;
+        }
+        
+        C = (FLATTENING / 16 * cosSquaredAlpha) *
+            (4 + FLATTENING *
+                (4 - 3 * cosSquaredAlpha)
+            );
+            
+        lambdaP = lambda;
+        lambda  = L +
+            (1 - C) *
+            FLATTENING * sinAlpha *
+                (sigma + C * sinSigma *
+                    (cos2SigmaM + C * cosSigma *
+                        (-1 + 2 * cos2SigmaM * cos2SigmaM)
+                    )
+                );
+        
+    } while ( (fabs(lambda - lambdaP) > .000000000001) &&
+              (++iterationCount       < ITERATION_LIMIT));
+                 
+    if (iterationCount == ITERATION_LIMIT) {
+        *distance = -1;
+        return;
+    }
+
+    uSquared = cosSquaredAlpha *
+            (MAJOR_AXIS_LENGTH * MAJOR_AXIS_LENGTH - MINOR_AXIS_LENGTH * MINOR_AXIS_LENGTH) /
+            (MINOR_AXIS_LENGTH * MINOR_AXIS_LENGTH);
+            
+    A = 1 + uSquared / 16384 *
+        (4096 + uSquared *
+            (-768 + uSquared *
+                (320 - 175 * uSquared)
+            )
+         );
+    
+    B = uSquared / 1024 *
+        (256 + uSquared *
+            (-128 + uSquared *
+                (74 - 47 * uSquared)
+            )
+        );
+        
+    deltaSigma = B * sinSigma *
+        (cos2SigmaM + B / 4 *
+            (cosSigma * (-1 + 2 * cos2SigmaM * cos2SigmaM) -
+            B / 6 * cos2SigmaM * 
+            (-3 + 4 * sinSigma * sinSigma) *
+            (-3 + 4 * cos2SigmaM * cos2SigmaM))
+        );
+        
+    *distance = MINOR_AXIS_LENGTH * A * (sigma - deltaSigma);
+    
+    *distance = ((int) ( (*distance) * 1000)) / 1000.0;
+    
+    *bearing = atan2(cosU2 * sinLambda, cosU1 * sinU2 - sinU1 * cosU2 * cosLambda);
+}
+
+
+
+
+
+
+ // extern c
 // vim:ts=4:sw=4:expandtab
