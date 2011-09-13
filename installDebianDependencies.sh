83e563cb527378c5682034ab70367e9bc9611c56
James Goppert <james.goppert@gmail.com>
2011-09-13 18:36:06 -0400

    Added stop block to end simulation.

--- installDebianDependencies.sh
+++ installDebianDependencies.sh
@@ -1,5 +1,5 @@
 echo you chose to install debian dependencies
 sudo apt-get install cmake libqt4-dev simgear-dev liblapack-dev libboost-all-dev libopenscenegraph-dev gfortran
 sudo apt-get install libgl1-mesa-dri libgl1-mesa-glx libglu1-mesa 
-sudo apt-get install automake libtool make expat libexpat1-dev
+sudo apt-get install automake libtool build-essential expat libexpat1-dev
 sudo apt-get install scicoslab-gtk
