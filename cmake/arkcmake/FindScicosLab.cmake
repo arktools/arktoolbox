02865366b6f2d69aecece2129a94fd3c5bf10271
James Goppert <james.goppert@gmail.com>
2012-03-16 16:08:23 -0400

    Fixed scicoslab find script.

--- cmake/arkcmake/FindScicosLab.cmake
+++ cmake/arkcmake/FindScicosLab.cmake
@@ -18,7 +18,9 @@ set(_SCICOSLAB_GUESS_SUFFIXES
     scicoslab-gtk-4.4
     scicoslab-gtk-4.4.1
     )
-
+foreach(_SCICOSLAB_GUESS_SUFFIX ${_SCICOSLAB_GUESS_SUFFIXES})
+    list(APPEND _SCICOSLAB_GUESS_SUFFIXES lib/${_SCICOSLAB_GUESS_SUFFIX})
+endforeach()
 
 # find scicos
 if (APPLE)
@@ -31,50 +33,50 @@ if (APPLE)
     if (RESULT) 
         MESSAGE(FATAL_ERROR "Could not locate 'ScicosLabGtk.app' - ${ERROR_MESSAGE}")
     endif (RESULT)
-    list(APPEND SCICOSLAB_GUESS_SUFFIXES ${_SCICOSLAB_APP})
+    list(APPEND _SCICOSLAB_GUESS_SUFFIXES ${_SCICOSLAB_APP})
 endif()
 
 # ScicosLab Root
 find_path(_SCICOSLAB_ROOT
     NAMES routines/scicos/scicos_block4.h
     PATHS ${_SCICOSLAB_EXTRA_SEARCH_PATHS}
-    SUFFIXES ${_SCICOSLAB_GUESS_SUFFIXES}
+    PATH_SUFFIXES ${_SCICOSLAB_GUESS_SUFFIXES}
     )
 
 # Include dir
 find_path(_SCICOSLAB_INCLUDE_DIR
     NAMES scicos/scicos_block4.h
     PATHS ${_SCICOSLAB_ROOT}
+    PATH_SUFFIXES routines
     NO_CMAKE_PATH
     NO_CMAKE_ENVIRONMENT_PATH
     NO_SYSTEM_ENVIRONMENT_PATH
     NO_CMAKE_SYSTEM_PATH
-    SUFFIXES routines
     )
 
 # Contrib dir
 find_path(SCICOSLAB_CONTRIB_DIR
     NAMES loader.sce
     PATHS ${_SCICOSLAB_ROOT}
+    PATH_SUFFIXES contrib
     NO_CMAKE_PATH
     NO_CMAKE_ENVIRONMENT_PATH
     NO_SYSTEM_ENVIRONMENT_PATH
     NO_CMAKE_SYSTEM_PATH
-    SUFFIXES contrib
     )
 
 # read the version
 if (EXISTS ${_SCICOSLAB_ROOT}/config/configuration)
-    file(READ ${_SCICOSLAB_INCLUDE_DIR}/arkcomm/config.h SCICOSLAB_CONFIG_FILE)
-    string(REGEX MATCH "PACKAGE_VERSION[ ]+:[ ]+([0-9]\\.[0-9]\\.[0-9])"
-        SCICOSLAB_VERSION_MATCH ${SCICOSLAB_CONFIG_FILE})
+    file(READ ${_SCICOSLAB_ROOT}/config/configuration _SCICOSLAB_CONFIG_FILE)
+    string(REGEX MATCH "PACKAGE_VERSION[ \t]+\\:([0-9]\\.[0-9]\\.[0-9])"
+        _SCICOSLAB_VERSION_MATCH ${_SCICOSLAB_CONFIG_FILE})
     set(SCICOSLAB_VERSION ${CMAKE_MATCH_1})
 endif()
 
 # handle arguments
 set(SCICOSLAB_INCLUDE_DIRS ${_SCICOSLAB_INCLUDE_DIR})
 find_package_handle_standard_args(ScicosLab
-    REQUIRED_VARS SCICOSLAB_CONTRIB_DIR SCICOSLAB_INCLUDE_DIRS
+    REQUIRED_VARS SCICOSLAB_INCLUDE_DIRS SCICOSLAB_CONTRIB_DIR 
     VERSION_VAR SCICOSLAB_VERSION
     )
 # vim:ts=4:sw=4:expandtab
