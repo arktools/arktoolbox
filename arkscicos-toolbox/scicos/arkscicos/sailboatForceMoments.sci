0b9a4edbfa2a0747d1fbd8c3d8529b3a1853c90e
James Goppert <james.goppert@gmail.com>
2012-03-24 21:44:19 -0400

    Sailboat fixed, but error with turning.

---
+++ arkscicos-toolbox/scicos/arkscicos/sailboatForceMoments.sci
@@ -0,0 +1,167 @@
+function [x,y,typ]=sailboatForceMoments(job,arg1,arg2)
+//
+// sailboatForceMoments.sci
+//
+// USAGE:
+//
+// Input:
+//  u1: winchPosition, rudderPosition
+//  u2: windSpeed, windDirection
+//  u3: U,W,theta,wy,V,phi,wx,psi,wz
+//  u4: sailPosition
+//
+// Output:
+//  y1: sailPosition, rudderPosition
+//  y2: F_b
+//  y3: M_b
+//  y4: U,W,theta,wy,V,phi,wx,psi,wz (u3, direct feed throguh)
+//  y5: alpha, apparentWindDir, LRudder, DRudder, apparentWindSpeed, relativeCourseOverGround
+//  y6: sailPositionRate
+//
+// AUTHOR:
+//
+// Copyright (C) James Goppert 2012
+//
+// This file is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This file is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+mode(-1);
+x=[];y=[];typ=[];
+
+select job
+	case 'plot' then
+	 	standard_draw(arg1)
+	case 'getinputs' then
+	 	[x,y,typ]=standard_inputs(arg1)
+	case 'getoutputs' then
+	 	[x,y,typ]=standard_outputs(arg1)
+	case 'getorigin' then
+	 	[x,y]=standard_origin(arg1)
+	case 'set' then
+		x=arg1;
+		graphics=arg1.graphics;
+        exprs=graphics.exprs
+		model=arg1.model;
+        while %t do
+            labels=[..
+                'env: density of air, kg/m^3';..
+                'env: density of water, kg/m^3';..
+                'sail: area, m^2';..
+                'sail: CD0';..
+                'sail: CD2';..
+                'sail: lift-curve slope';..
+                'sail: stall angle, rad';..
+                'sail: distance from mast to center of effort, m';..
+                'sail: distance from mast to center of mass, m';..
+                'rudder: distance to center of mass, m';..
+                'rudder: CD0';..
+                'rudder: CD2';..
+                'rudder: area, m^2';..
+                'rudder: stall angle, rad';..
+                'rudder: lift-curve slope';..
+                'hull: wetted area, m^2';..
+                'hull: CD';..
+                'hull: rotational damping coefficient'..
+                ];
+            [ok,rho,rhoW,s,cD0,cD2,cLAlpha,alphaStall,xSail,dSail,xRudder,cD0Rudder,..
+                cD2Rudder, sRudder, alphaRudderStall, cLAlphaRudder, sWater, cDWater, cWaterRot,..
+                exprs]=..
+                getvalue('Set Sailboat Parameters',labels,..
+                list(..
+                    'vec',1,'vec',1,'vec',1,'vec',1,'vec',1,..
+                    'vec',1,'vec',1,'vec',1,'vec',1,'vec',1,..
+                    'vec',1,'vec',1,'vec',1,'vec',1,'vec',1,..
+                    'vec',1,'vec',1,'vec',1),exprs);
+            if ~ok then break,end
+                graphics.exprs=exprs;
+
+            // set sizes
+            nOut=[2;3;3;9;6;1];
+            nIn=[2;2;9;1]
+
+            model.out=[nOut];
+            model.in=[nIn];
+            [model,graphics,ok]=check_io(model,graphics,nIn,nOut,[],[])
+            if ok then
+                model.rpar=[rho,rhoW,s,cD0,cD2,cLAlpha,alphaStall,xSail,dSail,xRudder,cD0Rudder,..
+                    cD2Rudder, sRudder, alphaRudderStall, cLAlphaRudder, sWater, cDWater, cWaterRot];
+                graphics.exprs=exprs;
+                x.graphics=graphics;
+                x.model=model;
+                break
+            end
+        end
+	case 'define' then
+		// set model properties
+		model=scicos_model();
+		model.sim=list('sci_sailboatForceMoments',4);
+
+		nOut=[2;3;3;9;6;1];
+		nIn=[2;2;9;1];
+
+		model.in=nIn;
+		model.out=nOut;
+		model.blocktype='c';
+		model.dep_ut=[%t %f];
+
+        // parameters
+        rho = 1.225;
+        rhoW = 1000;
+        s = 1;
+        cD0 = .1;
+        cD2 = .01;
+        cLAlpha = 2*%pi;
+        alphaStall = 20*%pi/180;
+        xSail = 0.2;
+        dSail = 0.2;
+        xRudder = 0.5;
+        cD0Rudder = 0.1;
+        cD2Rudder = 0.01;
+        sRudder = 0.05;
+        alphaRudderStall = 20*%pi/180;
+        cLAlphaRudder = 2;
+        sWater = 1;
+        cDWater = 0.03;
+        cWaterRot = 5;
+    
+        model.rpar=[rho,rhoW,s,cD0,cD2,cLAlpha,alphaStall,xSail,dSail,xRudder,cD0Rudder,..
+            cD2Rudder, sRudder, alphaRudderStall, cLAlphaRudder, sWater, cDWater, cWaterRot];
+		
+		// initialize strings for gui
+        exprs=[..
+            strcat(sci2exp(rho)),..
+            strcat(sci2exp(rhoW)),..
+            strcat(sci2exp(s)),..
+            strcat(sci2exp(cD0)),..
+            strcat(sci2exp(cD2)),..
+            strcat(sci2exp(cLAlpha)),..
+            strcat(sci2exp(alphaStall)),..
+            strcat(sci2exp(xSail)),..
+            strcat(sci2exp(dSail)),..
+            strcat(sci2exp(xRudder)),..
+            strcat(sci2exp(cD0Rudder)),..
+            strcat(sci2exp(cD2Rudder)),..
+            strcat(sci2exp(sRudder)),..
+            strcat(sci2exp(alphaRudderStall)),..
+            strcat(sci2exp(cLAlphaRudder)),..
+            strcat(sci2exp(sWater)),..
+            strcat(sci2exp(cDWater)),..
+            strcat(sci2exp(cWaterRot))];
+
+		// setup icon
+		gr_i=['xstringb(orig(1),orig(2),[''sailboat'';''force &'';''moments''],sz(1),sz(2),''fill'');']
+		x=standard_define([5 2],model,exprs,gr_i)
+	end
+endfunction
+
+// vim:ts=4:sw=4
---
+++ src/sci_sailboatForceMoments.cpp
@@ -0,0 +1,230 @@
+/*sci_sailboatForceMoments.cpp
+ * Copyright (C) James Goppert 2011
+ *
+ * This file is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This file is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * Input:
+ *  u1: winchPosition, rudderPosition
+ *  u2: windSpeed, windDirection
+ *  u3: U,W,thetai,wy,V,phi,wx,psi,wz
+ *  u4: sailPosition
+ *
+ * Output:
+ *  y1: sailPosition, rudderPosition
+ *  y2: F_b
+ *  y3: M_b
+ *  y4: U,W,theta,wy,V,phi,wx,psi,wz (u3, direct feed throguh)
+ *  y5: alpha, apparentWindDir, LRudder, DRudder, apparentWindSpeed, relativeCourseOverGround
+ *  y6: sailPositionRate
+ *
+ */
+
+#define BOOST_UBLAS_SHALLOW_ARRAY_ADAPTOR 1
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include "utilities.hpp"
+#include <stdexcept>
+
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/io.hpp>
+
+extern "C"
+{
+
+#include <scicos/scicos_block4.h>
+#include <math.h>
+#include "definitions.hpp"
+
+void sci_sailboatForceMoments(scicos_block *block, scicos::enumScicosFlags flag)
+{
+    // data
+    double * u1=(double*)GetInPortPtrs(block,1);
+    double * u2=(double*)GetInPortPtrs(block,2);
+    double * u3=(double*)GetInPortPtrs(block,3);
+    double * u4=(double*)GetInPortPtrs(block,4);
+    double * y1=(double*)GetOutPortPtrs(block,1);
+    double * y2=(double*)GetOutPortPtrs(block,2);
+    double * y3=(double*)GetOutPortPtrs(block,3);
+    double * y4=(double*)GetOutPortPtrs(block,4);
+    double * y5=(double*)GetOutPortPtrs(block,5);
+    double * y6=(double*)GetOutPortPtrs(block,6);
+    double * y7=(double*)GetOutPortPtrs(block,7);
+    double * rpar=(double*)GetRparPtrs(block);
+
+    // input
+    double winchPosition = u1[0]; // m/s
+    double rudderPosition = u1[1]; // rad
+
+    // wind
+    double windSpeed = u2[0]; // m/s
+    double windDir = u2[1]; // rad
+
+    // state variables
+    double U = u3[0]; // m/s
+    double W = u3[1]; // m/s
+    double theta = u3[2]; // rad
+    double wy = u3[3]; // rad/s
+    double V = u3[4]; // m/s
+    double phi= u3[5]; // rad
+    double wx = u3[6]; // rad/s
+    double psi = u3[7]; // rad
+    double wz = u3[8]; // rad/s
+
+    // sail position
+    double sailPosition = u4[0];
+
+    //handle flags
+    if (flag==scicos::computeOutput)
+    {
+        // environment constants
+        double rho = 1.225; // density of air, kg/m^3
+        double rhoW = 1000; // density of water, kg/m^3
+
+        // sail constants
+        double s = 1; // area of sail, m^2
+        double cD0 = 0.1; // zero order sail drag polar coefficient
+        double cD2 = 0.01; // 2nd order sail drag polar coefficient
+        double cLAlpha = 2*M_PI; // lift curve slope for sail
+        double alphaStall = 20*M_PI/180; // stall angle of attack for sail
+        double xSail = 0.2; // distance mast to sail center of effort, m
+        double dSail = 0.3; // distance from center of mass to mast, m
+
+        // rudder constants
+        double xRudder = 0.5; // distance from center of mass to rudder, m
+        double cD0Rudder = 0.1; // zero order rudder drag polar coefficient
+        double cD2Rudder = 0.01; // zero order rudder drag polar coefficient
+        double sRudder = 0.05; // area of rudder, m^2
+        double alphaRudderStall = 20*M_PI/180; // rudder stall angle, rad
+        double cLAlphaRudder = 2; // rudder lift curve slope
+
+        // hull constants
+        double sWater = 1; // wetted area of boat, m^2
+        double cDWater = 0.03; // drag coefficient, need to measure (can adjust using top speed)
+        double cWaterRot = 5; // coefficient of damping for spinning in the water
+
+        // local variables
+        double F_b[3] = {0,0,0}; // force in the body frame
+        double M_b[3] = {0,0,0}; // moment in the body frame
+        unsigned int i = 0; // counter
+
+        // sail lift/ drag
+        double apparentWind[2] = {0,0};
+        double relativeWindDir = windDir - psi;
+        while (relativeWindDir > M_PI) relativeWindDir = relativeWindDir - 2*M_PI;
+        while (relativeWindDir < -M_PI) relativeWindDir = relativeWindDir + 2*M_PI;
+        apparentWind[0] = windSpeed*cos(relativeWindDir) - U;
+        apparentWind[1] = windSpeed*sin(relativeWindDir) -V;
+        double apparentWindSpeed =sqrt(apparentWind[0]*apparentWind[0] + apparentWind[1]*apparentWind[1]);
+        double apparentWindDir = atan2(apparentWind[1],apparentWind[0]);
+        double alpha =  apparentWindDir -sailPosition;
+        while (alpha > M_PI) alpha = alpha - 2*M_PI;
+        while (alpha < -M_PI) alpha = alpha + 2*M_PI;
+        double cL = cLAlpha*alpha;
+        if  (alpha > alphaStall) cL = cLAlpha*alphaStall;
+        else if (alpha < -alphaStall) cL = -cLAlpha*alphaStall;
+        double cD = cD0 + cD2*cL*cL;
+        double q = 0.5*rho*apparentWindSpeed*apparentWindSpeed;
+        double L  = cL*q*s;
+        double D = cD*q*s;
+
+        // water drag
+        double groundSpeed =sqrt(U*U+V*V);
+        double relativeCourseOverGround = atan2(V,U);
+        double apparentWaterVelocityAtRudder[2] = {0,0};
+        apparentWaterVelocityAtRudder[0] = -U;
+        apparentWaterVelocityAtRudder[1] = -V; // + xRudder*wz;
+        double apparentWaterSpeedAtRudder =sqrt(apparentWaterVelocityAtRudder[0]*apparentWaterVelocityAtRudder[0] + 
+            apparentWaterVelocityAtRudder[1]*apparentWaterVelocityAtRudder[1]);
+        double apparentWaterDirAtRudder = atan2(apparentWaterVelocityAtRudder[1],apparentWaterVelocityAtRudder[0]);
+        double qWater = 0.5*rhoW*groundSpeed*groundSpeed; // wz has no effect at cm so using ground speed
+        double DWater = cDWater*qWater*sWater;
+
+        // rudder lift/ drag
+        double alphaRudder = apparentWaterDirAtRudder - rudderPosition;
+        while (alphaRudder > M_PI) alphaRudder = alphaRudder - 2*M_PI;
+        while (alphaRudder < -M_PI) alphaRudder = alphaRudder + 2*M_PI;
+        double cLRudder = cLAlphaRudder*alphaRudder;
+        if  (alphaRudder > alphaRudderStall) cLRudder = cLAlphaRudder*alphaRudderStall;
+        else if (alphaRudder < -alphaRudderStall) cLRudder = -cLAlphaRudder*alphaRudderStall;
+        double cDRudder = cD0Rudder + cD2Rudder*cLRudder*cLRudder;
+        double qWaterRudder = 0.5*rhoW*apparentWaterSpeedAtRudder*apparentWaterSpeedAtRudder; // wz has effect
+        double LRudder  = cLRudder*qWaterRudder*sRudder;
+        double DRudder = cDRudder*qWaterRudder*sRudder;
+
+        // hull spin damping
+        double WaterDamp = -cWaterRot*wz;
+        if (wz <0) WaterDamp = -WaterDamp;
+
+        // rudder moment
+        double MRudder = (LRudder*cos(rudderPosition) + DRudder * sin(rudderPosition))*xRudder;
+
+        // sail moment
+        double MSail = L*(-cos(sailPosition)*(dSail - xSail*cos(sailPosition))+sin(sailPosition)*xSail*cos(sailPosition)); 
+            + D*(-sin(sailPosition)*(dSail-xSail*cos(sailPosition))-xSail*cos(sailPosition)*cos(sailPosition));
+
+        // sum of forces
+        F_b[0] = L*sin(sailPosition) - D*cos(sailPosition) - DWater*cos(relativeCourseOverGround) + LRudder*sin(rudderPosition) - DRudder*cos(rudderPosition);
+        F_b[1] = 0; // assume no side silp
+        F_b[2] = 0; // assuming fixed in water
+
+        // sum of moments
+        M_b[0] = 0;
+        M_b[1] = 0;
+        M_b[2] = MSail + MRudder + WaterDamp;
+
+        // sail dynamics
+        double JSail = 0.1;
+        double winchMoment =  100*(fabs(sailPosition)-winchPosition*90*M_PI/180);
+        if (winchMoment < 0)  winchMoment = 0; // can't push
+        if (sailPosition > 0) winchMoment = -winchMoment;
+
+        double sailPositionRate = (L*xSail +winchMoment) / JSail;
+
+        // output assignment
+        y1[0] = sailPosition;
+        y1[1] = rudderPosition;
+
+        for(i=0;i<3;i=i+1) {
+            y2[i] = F_b[i];
+            y3[i] = M_b[i];
+        }
+        for(i=0;i<GetSizeOfIn(block,4);i=i+1) {
+            y4[i] = u3[i];
+        }
+
+        y5[0] = alpha;
+        y5[1] = apparentWindDir;
+        y5[2] = LRudder;
+        y5[3] = DRudder;
+        y5[4] = apparentWindSpeed;
+        y5[5] = relativeCourseOverGround;
+
+        y6[0] = sailPositionRate;
+    }
+    else if (flag==scicos::terminate)
+    {
+    }
+    else if (flag==scicos::initialize || flag==scicos::reinitialize)
+    {
+    }
+    else
+    {
+        std::cout << "unhandled block flag: " << flag << std::endl;
+    }
+}
+
+} // extern c
